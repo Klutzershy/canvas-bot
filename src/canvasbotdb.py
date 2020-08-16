ab1577ea01c9febc21dd6b3fe502b4c3bc7310d2
David Jefferson <davidmatthewjefferson@gmail.com>
2020-08-15 22:44:15 -0400

    return codes added, error handling added

--- src/canvasbotdb.py
+++ src/canvasbotdb.py
@@ -9,105 +9,151 @@ def create_connection(db_file):
     conn = None
     try:
         conn = sqlite3.connect(db_file)
-        return conn
+        return True
     except Error as e:
         print(e)
+        return False
 
 def create_table(create_table_sql):
     try:
         conn = sqlite3.connect(db_file)
         c = conn.cursor()
         c.execute(create_table_sql)
+        return True
     except Error as e:
         print(e)
+        return False
 
 def create_user(username, password):
     password = password.encode()
     salt = os.urandom(16)
     password_hash = hashlib.pbkdf2_hmac("sha256", password, salt, 100000)
     user = (username, password_hash, salt, "{short}@students.rowan.edu".format(short = username))
-    insert_user(user)
+    ret_val = insert_user(user)
+    return ret_val
 
 def create_preferences(username, reminder_time):
     row = select_user(username)
     if row is not None:
         preferences = (row[0], reminder_time)
-        insert_preferences(preferences) 
+        ret_val = insert_preferences(preferences) 
+    else:
+        ret_val = False
+    return ret_val
 
 def create_course(username, course_name) :
     row = select_user(username)
     if row is not None:
         course = (row[0], course_name)
-        insert_course(course)
+        ret_val = insert_course(course)
+    else:
+        ret_val = False
+    return ret_val
 
 def create_assignment(course_name, assignment_title, due_date):
     row = select_course(course_name)
     if row is not None:
         assignment = (row[0], assignment_title, due_date)
-        insert_assignment(assignment)
+        ret_val = insert_assignment(assignment)
+    else:
+        ret_val = False
+    return ret_val
 
 def insert_user(user):
-    conn = sqlite3.connect(db_file)
-    cur = conn.cursor()
-    row = select_user(user[0])
-    if row is None:
-        sql = ''' INSERT INTO users(username, password_hash, password_salt, email_address)
-              VALUES(?,?,?,?) '''
-        cur.execute(sql, user)
+    try:
+        conn = sqlite3.connect(db_file)
+        cur = conn.cursor()
+        row = select_user(user[0])
+        if row is None:
+            sql = ''' INSERT INTO users(username, password_hash, password_salt, email_address)
+                  VALUES(?,?,?,?) '''
+            val = user
+        else:
+            sql = ''' UPDATE users SET password_hash = ?, password_salt = ?
+                  WHERE username = ?'''
+            val = (user[1], user[2], user[0])
+        cur.execute(sql, val)          
         conn.commit()
+        return True
+    except Error as e:
+        print(e)
+        return False
 
 def insert_preferences(preferences):
-    conn = sqlite3.connect(db_file)
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM preferences WHERE user_id = {id}".format(id = preferences[0]))
-    row = cur.fetchone()
-    if row is None:
-        sql = ''' INSERT INTO preferences(user_id, reminder_time)
-              VALUES({id}, {rem_time}) '''.format(id = preferences[0], rem_time = preferences[1])
-    else:
-        sql = ''' UPDATE preferences SET reminder_time = {rem_time} 
-              WHERE user_id = {id}'''.format(rem_time = preferences[1], id = preferences[0])
-    cur.execute(sql)
-    conn.commit()
-
-def insert_course(course):
-    conn = sqlite3.connect(db_file)
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM courses WHERE user_id = {id} AND name = \"{name}\"".format(id = course[0], name = course[1]))
-    row = cur.fetchone()
-    if row is None:
-        sql = ''' INSERT INTO courses(user_id, name)
-              VALUES({id}, \"{course_name}\") '''.format(id = course[0], course_name = course[1])
+    try:
+        conn = sqlite3.connect(db_file)
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM preferences WHERE user_id = {id}".format(id = preferences[0]))
+        row = cur.fetchone()
+        if row is None:
+            sql = ''' INSERT INTO preferences(user_id, reminder_time)
+                  VALUES({id}, {rem_time}) '''.format(id = preferences[0], rem_time = preferences[1])
+        else:
+            sql = ''' UPDATE preferences SET reminder_time = {rem_time} 
+                  WHERE user_id = {id}'''.format(rem_time = preferences[1], id = preferences[0])
         cur.execute(sql)
         conn.commit()
+        return True
+    except Error as e:
+        print(e)
+        return False
+
+def insert_course(course):
+    try:
+        conn = sqlite3.connect(db_file)
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM courses WHERE user_id = {id} AND name = \"{name}\"".format(id = course[0], name = course[1]))
+        row = cur.fetchone()
+        if row is None:
+            sql = ''' INSERT INTO courses(user_id, name)
+                  VALUES({id}, \"{course_name}\") '''.format(id = course[0], course_name = course[1])
+            cur.execute(sql)
+            conn.commit()
+        return True
+    except Error as e:
+        print(e)
+        return False
 
 def insert_assignment(assignment):
-    conn = sqlite3.connect(db_file)
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM assignments WHERE course_id = {id} AND title = \"{name}\"".format(id = assignment[0], name = assignment[1]))
-    row = cur.fetchone()
-    if row is None:
-        sql = ''' INSERT INTO assignments(course_id, title, due_date)
-              VALUES({id}, \"{title}\", \"{due_date}\") '''.format(id = assignment[0], title = assignment[1], due_date = assignment[2])
-    else:
-        sql = ''' UPDATE assignments SET due_date = \"{due_date}\" 
-              WHERE course_id = {id} AND title = \"{title}\"'''.format(due_date = assignment[2], id = assignment[0], title = assignment[1])
-    cur.execute(sql)
-    conn.commit()
+    try:
+        conn = sqlite3.connect(db_file)
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM assignments WHERE course_id = {id} AND title = \"{name}\"".format(id = assignment[0], name = assignment[1]))
+        row = cur.fetchone()
+        if row is None:
+            sql = ''' INSERT INTO assignments(course_id, title, due_date)
+                  VALUES({id}, \"{title}\", \"{due_date}\") '''.format(id = assignment[0], title = assignment[1], due_date = assignment[2])
+        else:
+            sql = ''' UPDATE assignments SET due_date = \"{due_date}\" 
+                  WHERE course_id = {id} AND title = \"{title}\"'''.format(due_date = assignment[2], id = assignment[0], title = assignment[1])
+        cur.execute(sql)
+        conn.commit()
+        return True
+    except Error as e:
+        print(e)
+        return False
 
 def select_user(username):
-    conn = sqlite3.connect(db_file)
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM users WHERE username = \"{uname}\"".format(uname = username))
-    row = cur.fetchone()
-    return row
+    try:
+        conn = sqlite3.connect(db_file)
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM users WHERE username = \"{uname}\"".format(uname = username))
+        row = cur.fetchone()
+        return row
+    except Error as e:
+        print(e)
+        return None
 
 def select_course(course_name):
-    conn = sqlite3.connect(db_file)
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM courses WHERE name = \"{cname}\"".format(cname = course_name))
-    row = cur.fetchone()
-    return row
+    try:
+        conn = sqlite3.connect(db_file)
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM courses WHERE name = \"{cname}\"".format(cname = course_name))
+        row = cur.fetchone()
+        return row
+    except Error as e:
+        print(e)
+        return None
 
 def user_registered(username):
     user = select_user(username)
@@ -150,6 +196,24 @@ def get_user_preferences(username):
         return preferences
     else:
         return None
+
+def clean():
+    try:
+        conn = sqlite3.connect(db_file)
+        cur = conn.cursor()
+        cur.execute("DELETE FROM users;")
+        cur.execute("DELETE FROM preferences;")
+        cur.execute("DELETE FROM courses;")
+        cur.execute("DELETE FROM assignments;")
+        cur.execute("DROP TABLE users;")
+        cur.execute("DROP TABLE preferences;")
+        cur.execute("DROP TABLE courses;")
+        cur.execute("DROP TABLE assignments;")
+        return True
+    except Error as e:
+        print(e)
+        return False
+    
 
 def init():
     # create a database connection
