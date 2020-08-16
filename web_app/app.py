c90f11a9aaaa498958dd45f7bd6d067813ec6d2b
Brendan Armstrong <armstr27@gmail.com>
2020-08-16 16:59:06 -0400

    Updated app routing

--- web_app/app.py
+++ web_app/app.py
@@ -1,18 +1,28 @@
+import json
 
-# from flask import Flask,request,render_template
-# #import cb_controller
-# #from CBController import cb_controller
+from flask import Flask, request, render_template, Response
+from flask_cors import CORS
+#import cb_controller
+#from CBController import cb_controller
 
-# #Initialize Flask App
-# app = Flask(__name__)
+#Initialize Flask App
+app = Flask(__name__)
+CORS(app)
 
-# @app.route('/')
-# def index():
-#     return render_template("index.html")
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
 
-# #@app.route('/register_user')
+@app.route('/register_user', methods=['POST'])
+def register():
+    post_data = request.get_json() #This gets the json object sent by the post request
 
-# #app.register_blueprint(cb_controller, url_prefix="/cb_controller")
+    print(post_data)
+
+    response = {"success": True}
+    return Response(json.dumps(response), mimetype='application/json')
+
+#app.register_blueprint(cb_controller, url_prefix="/cb_controller")
 
 
-# app.run(debug=True)
+app.run(debug=True)
